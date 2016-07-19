976fb3cde2ab9261f59ff7156b2f9c78539d8fa7
Ratts <ratts@DoutorPcs-iMac-2.local>
2016-07-19 11:39:58 -0300

    Layout theme base

---
+++ README.md
@@ -0,0 +1,1 @@
+# hudsonbrendon.github.io
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,3 @@
-
 var gulp        = require('gulp'),
     plumber     = require('gulp-plumber'),
     browserSync = require('browser-sync'),
@@ -12,11 +11,13 @@ var gulp        = require('gulp'),
     imagemin    = require('gulp-imagemin'),
     cp          = require('child_process');
 
-
 var messages = {
     jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
 };
 
+/**
+ * Build the Jekyll Site
+ */
 gulp.task('jekyll-build', function (done) {
     browserSync.notify(messages.jekyllBuild);
     return cp.spawn('jekyll', ['build'], {stdio: 'inherit'})
@@ -24,15 +25,14 @@ gulp.task('jekyll-build', function (done) {
 });
 
 /**
- * atualiza a página
+ * Rebuild Jekyll & do page reload
  */
 gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
     browserSync.reload();
 });
 
 /**
- * Espera até que o jekyll-build seja executado e então levanta o
- * servidor utilizando o _site como pasta raiz
+ * Wait for jekyll-build, then launch the Server
  */
 gulp.task('browser-sync', ['jekyll-build'], function() {
     browserSync({
@@ -42,6 +42,9 @@ gulp.task('browser-sync', ['jekyll-build'], function() {
     });
 });
 
+/**
+ * Stylus task
+ */
 gulp.task('stylus', function(){
         gulp.src('src/styl/main.styl')
         .pipe(plumber())
@@ -54,6 +57,9 @@ gulp.task('stylus', function(){
         .pipe(gulp.dest('assets/css'))
 });
 
+/**
+ * Javascript Task
+ */
 gulp.task('js', function(){
     return gulp.src('src/js/**/*.js')
         .pipe(plumber())
@@ -62,18 +68,29 @@ gulp.task('js', function(){
         .pipe(gulp.dest('assets/js/'))
 });
 
+/**
+ * Imagemin Task
+ */
 gulp.task('imagemin', function() {
-    return gulp.src('src/img/**/*')
+    return gulp.src('src/img/**/*.{jpg,png,gif}')
         .pipe(plumber())
         .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
         .pipe(gulp.dest('assets/img/'));
 });
 
+/**
+ * Watch stylus files for changes & recompile
+ * Watch html/md files, run jekyll & reload BrowserSync
+ */
 gulp.task('watch', function () {
     gulp.watch('src/styl/**/*.styl', ['stylus']);
     gulp.watch('src/js/**/*.js', ['js']);
      gulp.watch('src/img/**/*.{jpg,png,gif}', ['imagemin']);
-    gulp.watch(['index.html', '_includes/*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
+    gulp.watch(['*.html','index.html', '_includes/*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
 });
 
+/**
+ * Default task, running just `gulp` will compile the sass,
+ * compile the jekyll site, launch BrowserSync & watch files.
+ */
 gulp.task('default', ['js', 'stylus', 'imagemin', 'browser-sync', 'watch']);
