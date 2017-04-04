18ddd33bf672a0eadd3266e02275efaef6d86378
Lucas Miquéias <lucasmiqueias1@gmail.com>
2017-04-04 00:15:34 -0300

    Alterações em progresso

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,9 @@
 
 var gulp        = require('gulp'),
-    plumber     = require('gulp-plumber'),
+    sass     = require('sass'),
     browserSync = require('browser-sync'),
-    stylus      = require('gulp-stylus'),
-    uglify      = require('gulp-uglify'),
     concat      = require('gulp-concat'),
-    jeet        = require('jeet'),
-    rupture     = require('rupture'),
-    koutoSwiss  = require('kouto-swiss'),
-    prefixer    = require('autoprefixer-stylus'),
-    imagemin    = require('gulp-imagemin'),
-    cp          = require('child_process');
+    imagemin    = require('gulp-imagemin');
 
 
 var messages = {
@@ -42,38 +35,16 @@ gulp.task('browser-sync', ['jekyll-build'], function() {
     });
 });
 
-gulp.task('stylus', function(){
-        gulp.src('src/styl/main.styl')
-        .pipe(plumber())
-        .pipe(stylus({
-            use:[koutoSwiss(), prefixer(), jeet(),rupture()],
-            compress: true
-        }))
-        .pipe(gulp.dest('_site/assets/css/'))
-        .pipe(browserSync.reload({stream:true}))
-        .pipe(gulp.dest('assets/css'))
+gulp.task('sass', function () {
+ return gulp.src('_/assets/_sass/base.scss')
+   .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+   .pipe(gulp.dest('_site/dist/css'));
 });
 
-gulp.task('js', function(){
-    return gulp.src('src/js/**/*.js')
-        .pipe(plumber())
-        .pipe(concat('main.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('assets/js/'))
-});
-
-gulp.task('imagemin', function() {
-    return gulp.src('src/img/**/*')
-        .pipe(plumber())
-        .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
-        .pipe(gulp.dest('assets/img/'));
-});
 
 gulp.task('watch', function () {
-    gulp.watch('src/styl/**/*.styl', ['stylus']);
-    gulp.watch('src/js/**/*.js', ['js']);
-     gulp.watch('src/img/**/*.{jpg,png,gif}', ['imagemin']);
+    gulp.watch('_/assets/_sass/base.scss', ['sass']);
     gulp.watch(['index.html', '_includes/*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
 });
 
-gulp.task('default', ['js', 'stylus', 'imagemin', 'browser-sync', 'watch']);
+gulp.task('default', ['sass', 'browser-sync', 'watch']);
