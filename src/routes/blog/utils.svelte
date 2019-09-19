b50fd75782f4e0fbc5860f38444195be831164d6
Lucas Miqueias <contato@lucasmiqueias.com.br>
2019-09-19 14:34:59 -0300

    add utils blog and change netlify cms

---
+++ src/routes/blog/utils.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { onMount } from "svelte";
+
+  export let posts = [];
+
+  onMount(async function() {
+    const response = await fetch(`blog.json`);
+    const json = await response.json();
+    posts = json;
+  });
+</script>
+
+<slot {posts} />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,6 @@
 <script>
   import { onMount } from "svelte";
+  import Posts from "./blog/utils.svelte";
 
   onMount(() => {
     if (window.netlifyIdentity) {
@@ -14,42 +15,6 @@
   });
 </script>
 
-<style>
-  h1,
-  figure,
-  p {
-    text-align: center;
-    margin: 0 auto;
-  }
-
-  h1 {
-    font-size: 2.8em;
-    text-transform: uppercase;
-    font-weight: 700;
-    margin: 0 0 0.5em 0;
-  }
-
-  figure {
-    margin: 0 0 1em 0;
-  }
-
-  img {
-    width: 100%;
-    max-width: 400px;
-    margin: 0 0 1em 0;
-  }
-
-  p {
-    margin: 1em auto;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      font-size: 4em;
-    }
-  }
-</style>
-
 <svelte:head>
   <title>Sapper project template</title>
   <script src="https://identity.netlify.com/v1/netlify-identity-widget.js">
@@ -57,15 +22,11 @@
   </script>
 </svelte:head>
 
-<h1>Great success!</h1>
-
-<figure>
-  <img alt="Borat" src="great-success.png" />
-  <figcaption>HIGH FIVE!</figcaption>
-</figure>
-
-<p>
-  <strong>
-    Try editing this file (src/routes/index.svelte) to test live reloading.
-  </strong>
-</p>
+<span>Lucas Miqueias</span>
+<Posts let:posts>
+  {#each posts as post}
+    <li>
+      <a href="blog/{post.slug}">{post.title}</a>
+    </li>
+  {/each}
+</Posts>
