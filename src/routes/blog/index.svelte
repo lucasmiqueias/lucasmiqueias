83bb9949c24cbe86fec34d36b6c8974b0cdd7d01
Lucas Miqueias <contato@lucasmiqueias.com.br>
2019-09-19 19:15:39 -0300

    updated index blog

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -1,21 +1,5 @@
 <script context="module">
   export function preload({ params, query }) {
-    return this.fetch(`blog.json`)
-      .then(r => r.json())
-      .then(datas => {
-        return { datas };
-      });
+    return this.fetch(`blog.json`);
   }
 </script>
-
-<script>
-  export let datas;
-</script>
-
-<div>
-  {#each datas as data}
-    <li>
-      <a rel="prefetch" href="blog/{data.slug}">{data.title}</a>
-    </li>
-  {/each}
-</div>
